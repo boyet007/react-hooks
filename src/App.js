19344565132cc4475801fbe8b5d7d63e5126699c
Philip <boyet007@gmail.com>
2019-10-15 01:03:05 +0700

    fresh install

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import Navbar from './components/Navbar'
-import BookList from './components/BookList'
-import './index.css'
-import ThemeContextProvider from './context/ThemeContext';
-import ThemeToggle from './components/ThemeToggle';
-import AuthContextProvider from './context/AuthContext';
-import BookContextProvider from './context/BookContext';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <AuthContextProvider>
-          <Navbar />
-          <BookContextProvider>
-          <BookList />
-          </BookContextProvider>
-          <ThemeToggle />
-        </AuthContextProvider>
-      </ThemeContextProvider>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
