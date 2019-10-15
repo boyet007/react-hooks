e0b8bfbd8c17de21f24897273f6b7b97bdbec07a
Philip Loa <boyet007@gmail.com>
2019-10-15 12:20:52 +0700

    19 - Reducers, Actions, State

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-import React from 'react';
-import BookContextProvider from './context/BookContext'
-import Navbar from './components/Navbar';
-import BookList from './components/BookList'
-import BookForm from './components/BookForm'
-import './index.css'
+import React, { createContext, useState, useReducer } from 'react';
+
+//const AgeContext = createContext()
+
+const ageReducer = ( state, action ) => {
+  switch(action.type) {
+    case 'ADD_ONE':
+      return state + 1
+    case 'ADD_FIVE' : 
+      return state + 5
+    case 'ADD_NUM' :
+      return state + action.num
+    default :
+      return state
+  }
+}
+
+dispatch({ type: 'ADD_ONE' })
+dispatch({ type: 'ADD_NUM', num: 2 })
+
+const AgeContextProvider = (props) => {
+  // const [age, setAge] = useState(20)
+  const { age, dispatch } = useReducer(ageReducer, 20)
+
+  // const addOneToAge = () => setAge( age + 1 )
+  // const addFiveToAge =  () => setAge( age + 5 )
+  // const addNumToAge = ( num ) => setAge( age + num )
+
+
+
+  return ( 
+    <AgeContext.Provider value={ age, addOneToAge, addFiveToAge, addNumToAge}>
+      { props.children }
+    </AgeContext.Provider>
+   )
+
+}
+
 
 function App() {
   return (
     <div className="App">
-      <BookContextProvider>
-        <Navbar />
-        <BookList />
-        <BookForm />
-      </BookContextProvider>
+      
     </div>
   );
 }
